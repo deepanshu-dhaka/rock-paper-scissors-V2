04410c3865bb99afb8d94eae7019c641d072bde7
deepanshu-dhaka <dhakadeepanshu30@gmail.com>
2026-05-29 07:42:40 +0000

    Make getComputerChoice function
    Math.floor and Math.random are used inside this function to get
    random choices among 0,1 and 2 all inclusive. These random choices are
    used to determine the choice among 'rock', 'paper', and 'scissors'.
    
    Overall, this function generates computer choice for the
    rock-paper-scissors game.

---
+++ script.js
@@ -0,0 +1,13 @@
+// returns computer choice from 'rock', 'paper' or 'scissors'
+function getComputerChoice(){
+    // generate random number among 0,1 and 2 all inclusive
+    random_comp_choice = Math.floor(Math.random() * 3)
+
+    if (random_comp_choice === 0){
+        return 'rock';
+    } else if (random_comp_choice === 1){
+        return 'paper';
+    } else{
+        return 'scissors';
+    }
+}
