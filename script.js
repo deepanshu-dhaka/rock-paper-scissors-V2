402cb82d4efa18ba17a83bc3e6db7856cbb193b8
deepanshu-dhaka <dhakadeepanshu30@gmail.com>
2026-06-06 07:36:09 +0000

    Complete version 1 of the game

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 // returns computer choice from 'rock', 'paper' or 'scissors'
 function getComputerChoice() {
     // generate random number among 0,1 and 2 all inclusive
@@ -11,17 +13,6 @@ function getComputerChoice() {
         return "scissors";
     }
 }
-
-// get the human or user choice
-function getHumanChoice() {
-    human_choice = prompt(
-        "Enter your choice: 'rock', 'paper' or 'scissors'",
-    ).toLowerCase();
-    return human_choice;
-}
-
-let humanChoice = getHumanChoice();
-let computerChoice = getComputerChoice();
 
 
 
@@ -39,6 +30,10 @@ function playRound(humanChoice, computerChoice) {
     ) {
         console.log(`You win! ${humanChoice} beat ${computerChoice}`);
         humanScore += 1;
+        let str = `You win! ${humanChoice} beat ${computerChoice}`;
+        printResult(str);
+        printHumanScore(humanScore);
+        printComputerScore(computerScore);
     }
     // all the conditions in which computer will win
     else if (
@@ -48,9 +43,68 @@ function playRound(humanChoice, computerChoice) {
     ) {
         console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
         computerScore += 1;
+        let str = `You lose! ${computerChoice} beats ${humanChoice}`;
+        printResult(str);
+        printHumanScore(humanScore);
+        printComputerScore(computerScore);
     } else {
         console.log("Draw! Nobody wins");
+        let str = "Draw! Nobody wins";
+        printResult(str);
+        printHumanScore(humanScore);
+        printComputerScore(computerScore);
+    }
+
+    if (humanScore === 5) {
+        printResult("You're the overall winner!")
+    }
+    if (computerScore === 5) {
+        printResult("You lost! Computer won the overall game!")
     }
 }
 
-playRound(humanChoice, computerChoice);
+
+function playGame(event) {
+
+    if (humanScore === 5 || computerScore === 5) {
+        return;
+    };
+    let humanChoice = getHumanChoice(event);
+    let computerChoice = getComputerChoice();
+    console.log("Human Choice:", humanChoice)
+    console.log("Computer Choice:", computerChoice)
+
+
+    playRound(humanChoice, computerChoice);
+
+    console.log(humanScore, computerScore);
+    printResult();
+
+}
+
+function printResult(str) {
+    if (str) document.getElementById("result-display-paragraph").innerText = str;
+}
+
+function printHumanScore(num) {
+    document.getElementById("human-score-display-paragraph").innerText = num;
+}
+
+function printComputerScore(num) {
+    document.getElementById("computer-score-display-paragraph").innerText = num;
+}
+
+function getHumanChoice(event) {
+    if (event.target.className === "rock-image") return "rock";
+    else if (event.target.className === "paper-image") return "paper";
+    else if (event.target.className === "scissors-image") return "scissors";
+}
+
+
+let clickedImages = document.querySelectorAll("img");
+clickedImages.forEach(function (image) {
+    image.addEventListener("click", playGame)
+})
+
+
+
